be9c5f4f17b56d4e1cb604e7dfe77eee9dcee1d1
Luwa Kuns <luwakuns@gmail.com>
2021-01-26 19:42:09 -0600

    First exercise js

---
+++ Ejercicio0/main.js
@@ -0,0 +1,27 @@
+console.log("Hola mundo desde js");
+
+var saludo = "Hola, me llamo celeste";
+console.log(saludo);
+
+var saludo = "Holi 2";
+console.log(saludo);
+
+/* Hasta aquí vemos el historico de la variable */
+
+var a = 5;
+var b = 10;
+var c = 15;
+
+var p1 = (3 + b) * c / a * 2;
+var p2 = ((a+b*c) / 5) * 2;
+var p3 = b/ a + 2 * c;
+var p4 = (a+b+c/c) * a;
+var p5 = ((3*a)/c) + a+b+c;
+var p6 = a - (b+c) * a /1;
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
+console.log(p4);
+console.log(p5);
+console.log(p6);
